7eafcc8e38e70a42c30f3c4e5504d41303c65537
dkarsh <dkarsh@roundarchisolab.com>
2015-08-28 14:35:51 -0400

    readyforAWSAPI

---
+++ Example/NSString+MD5.h
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5.h
+//  amplfy
+//
+//  Created by DKarsh on 8/28/15.
+//  Copyright (c) 2015 Kickflip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+@end
---
+++ Example/NSString+MD5.m
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5.m
+//  amplfy
+//
+//  Created by DKarsh on 8/28/15.
+//  Copyright (c) 2015 Kickflip. All rights reserved.
+//
+
+#import "NSString+MD5.h"
+
+@implementation NSString (MD5)
+
+@end
